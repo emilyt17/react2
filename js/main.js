29a10844769acd259e1098b904d2f076fbc2ddca
emilyt17 <emilytroconis@gmail.com>
2022-01-26 12:07:56 -0300

    second

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const email = document.getElementById('exampleInputEmail1').value;
-const password = document.getElementById('exampleInputPassword1').value;
-
-$(document).ready(function () {
-    boton.click(function (e) { 
-        e.preventDefault();
-        console.log(email , password);
-    });
-});
